2db555875216fe829b5442593efc995ce9263afc
aizaz <aizaznetwork@gmail.com>
2019-05-07 09:16:29 +0500

    Merge remote-tracking branch 'github/master'
    # Conflicts:
    #	app/build.gradle

---
+++ app/src/main/java/com/dcservicez/a247services/Select_service.java
@@ -0,0 +1,13 @@
+package com.dcservicez.a247services;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Select_service extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_service);
+    }
+}
